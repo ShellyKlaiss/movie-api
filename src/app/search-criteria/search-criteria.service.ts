bea7d6ff286f16c0442e51d2866d224211cd55a7
shelly <shellynacole@yahoo.com>
2019-11-04 20:34:57 -0500

    search functional

--- src/app/search-criteria/search-criteria.service.ts
+++ src/app/search-criteria/search-criteria.service.ts
@@ -14,5 +14,6 @@ export class SearchCriteriaService {
   console.log(subName)
 
   return this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=c5f5e747de4ed4d4e94401b73a5737ac&language=en-US&page=1&include_adult=false&query=${subName}`);
+
 }
 }
